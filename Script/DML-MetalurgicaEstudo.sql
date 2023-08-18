2295911aa1c6e971ba07f2bfd29a18ac324d2219
Lucas Santos Medina de Oliveira <lucaso@maceng.mac.engenharia.com.br>
2023-08-18 16:01:22 -0300

    inner joini

--- Script/DML-MetalurgicaEstudo.sql
+++ Script/DML-MetalurgicaEstudo.sql
@@ -1,3 +1,24 @@
 INSERT INTO Lm_TipoUsuario(Nm_Titulo, Dt_Cadastro, Fl_Ativo)
 VALUES ('adm', '2023-01-01', 1), ('colaborador','2023-01-01', 1)
 
+SELECT * FROM LM_TipoUsuario
+
+SELECT * FROM LM_Elemento
+
+SELECT * FROM LM_Produto
+
+SELECT * FROM LM_Usuario
+
+SELECT * FROM LM_Embalagem
+
+SELECT * FROM LM_Empresa
+
+SELECT * FROM LM_Lote
+
+SELECT LM_Lote.Id_Lote,LM_Lote.Dt_Cadastro, LM_Lote.Nm_MetodologiaAnaliseGranumetrica, LM_Lote.Ds_observacoes, LM_Produto.Nm_Nome AS NomeProduto, LM_Embalagem.Nm_Nome AS NomeEmbalagem, LM_Empresa.Nm_Nome AS NomeEmpresa FROM LM_Lote 
+INNER JOIN LM_Produto ON LM_Produto.Id_Produto = LM_Lote.Id_Produto
+INNER JOIN LM_Embalagem ON LM_Embalagem.Id_Embalagem = LM_Lote.Id_Embalagem
+INNER JOIN LM_Empresa ON LM_Empresa.Id_Empresa = LM_Lote.Id_Empresa
+where LM_Lote.Id_Lote = 4
+
+
